f3c3dd809e1adbd6701277796b0e73dfbad02242
Marc Ole Bulling <Marc-Ole@gmx.de>
2020-03-26 12:10:13 +0100

    Added unstable versions for aarch64/armhf, reduced pm.maxchildren variable

--- buildscript.sh
+++ buildscript.sh
@@ -7,7 +7,7 @@ BACKTITLE="Barcode Buddy"
 TITLE="Docker Build Script"
 MENU="Choose one of the following options:"
 
-OPTIONS=(1 "Build and release dev image"
+OPTIONS=(1 "Build and release dev images"
          2 "Build and release all images")
 
 CHOICE=$(dialog --clear \
@@ -21,10 +21,18 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            echo "Building dev image"
+            echo "Building dev images"
+	    cp /usr/bin/qemu-arm-static .
+	    cp /usr/bin/qemu-aarch64-static .
 	    docker build --no-cache --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t f0rc3/barcodebuddy-docker:latest-dev -f Dockerfile.dev .
-            echo "Pushing dev image"
+	    docker build --no-cache --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t f0rc3/barcodebuddy-docker:arm64v8-latest-dev -f Dockerfile.dev.aarch64 .
+	    docker build --no-cache --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t f0rc3/barcodebuddy-docker:arm32v7-latest-dev -f Dockerfile.dev.armhf .
+            echo "Pushing dev images"
 	    docker push f0rc3/barcodebuddy-docker:latest-dev
+	    docker push f0rc3/barcodebuddy-docker:arm64v8-latest-dev
+	    docker push f0rc3/barcodebuddy-docker:arm32v7-latest-dev
+	    rm qemu-arm-static
+	    rm qemu-aarch64-static
             ;;
         2)
             echo "Building all images"
@@ -36,10 +44,15 @@ case $CHOICE in
 	    docker build --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VERSION="${version}" -t f0rc3/barcodebuddy-docker:latest -f Dockerfile .
 	    docker build --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VERSION="${version}" -t f0rc3/barcodebuddy-docker:arm64v8-latest -f Dockerfile.aarch64 .
 	    docker build --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VERSION="${version}" -t f0rc3/barcodebuddy-docker:arm32v7-latest -f Dockerfile.armhf .
-	    docker push f0rc3/barcodebuddy-docker:latest-dev
+	    docker build --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t f0rc3/barcodebuddy-docker:arm64v8-latest-dev -f Dockerfile.dev.aarch64 .
+	    docker build --pull --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t f0rc3/barcodebuddy-docker:arm32v7-latest-dev -f Dockerfile.dev.armhf .
+            echo "Pushing all images"
 	    docker push f0rc3/barcodebuddy-docker:latest
+	    docker push f0rc3/barcodebuddy-docker:latest-dev
 	    docker push f0rc3/barcodebuddy-docker:arm64v8-latest
 	    docker push f0rc3/barcodebuddy-docker:arm32v7-latest
+	    docker push f0rc3/barcodebuddy-docker:arm64v8-latest-dev
+	    docker push f0rc3/barcodebuddy-docker:arm32v7-latest-dev
 	    rm qemu-arm-static
 	    rm qemu-aarch64-static
             ;;
